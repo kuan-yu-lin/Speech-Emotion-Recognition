abdbfdae552ad17d36339defc96c9b9685e19513
Kuan-Yu Lin <75708899+kuan-yu-lin@users.noreply.github.com>
2023-04-19 19:19:32 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,36 +14,15 @@ release = '15-03-2023'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_book_theme',
-    "ablog",
-    "myst_nb",
-    "numpydoc",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinxcontrib.youtube",
-    "sphinx_copybutton",
-    "sphinx_design",
-    "sphinx_examples",
-    "sphinx_tabs.tabs",
-    "sphinx_thebe",
-    "sphinx_togglebutton",
-    "sphinxcontrib.bibtex",
-    "sphinxext.opengraph",
-    # For the kitchen sink
-    "sphinx.ext.todo",
-]
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_title = "Speech Emotion Recognition"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
